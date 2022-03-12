0d6bfc0d980270aa58e124b4f9bfde4eeaa3e1dc
Emiel Vereycken <emielvereycken8@gmail.com>
2022-03-12 19:41:28 +0100

    Solve project 04

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,88 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(INFINITE)
+
+	// read keyboard register
+	@KBD
+	D = M
+
+	@WHITE
+	D; JEQ // whiten screen if keyboard register is 0 (no key pressed)
+
+	@BLACK
+	0; JMP // Make screen black otherwise (keyboard register != 0)	 
+
+(WHITE)
+	@8192
+	D = A // D = 8192
+	@i
+	M = D //8192 == (256*512)/16 (number of rows in the memory map)
+
+	@addr
+	M = 0
+
+	@WRITEWHITE
+	0;JMP
+
+(BLACK)
+	@8192
+	D = A
+	@i
+	M = D
+	
+	@addr
+	M = 0
+	
+	@WRITEBLACK
+	0;JMP
+
+(WRITEWHITE)
+
+	@i
+	D = M
+	@INFINITE
+	D;JEQ // if i == 0; return infinite
+
+	@i
+	M = M - 1
+	
+	@SCREEN
+	D = A
+	@addr
+	D = D + M
+	A = D
+	M = 0
+
+	@addr
+	M = M + 1
+
+	@WRITEWHITE
+	0;JMP
+
+
+(WRITEBLACK)
+
+	@i
+	D = M
+	@INFINITE
+	D;JEQ // if i == 0; return infinite
+	
+	@i
+	M = M - 1
+
+	@SCREEN
+	D = A
+	@addr
+	D = D + M
+	A = D
+	M = -1
+	
+	@addr
+	M = M + 1
+
+	@WRITEBLACK
+	0;JMP
+	
+
--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,48 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+	// i = R1
+	@R1
+	D = M
+	@i
+	M = D
+
+	// res = 0
+	@res
+	M = 0
+
+(LOOP)
+	@i
+	D=M
+
+	@RESULT
+	D;JEQ //i==0 -> end loop
+
+	@i
+	M=D-1 //i=i-1
+
+	//res = res + R0
+	@res
+	D = M
+	@R0
+	D = D + M
+	@res
+	M = D
+	
+	@LOOP
+	0;JMP
+		
+	
+
+(RESULT)
+	// put result in R2
+	@res
+	D = M
+	@R2
+	M = D
+(END)
+	@END
+	0;JMP
+
+	
